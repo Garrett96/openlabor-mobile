2d22821179f5a7d7b4e2c2630c5b4cdc35d4a9c1
Garrett <46694517+Garrett96@users.noreply.github.com>
2025-07-28 12:22:10 -0400

    Revert "gradle hell"
    This reverts commit 035138335fa09cced3143f8d0bd05b16bdb06a33.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
         freeCompilerArgs = listOf(
             "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
             "-opt-in=com.marosseleng.compose.material3.datetimepickers.date.ui.dialog.ExperimentalMaterial3DatePickerApi",
